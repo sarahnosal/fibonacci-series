cab772852cd4c0c5a6779f0326279bfaa970622d
Sarah Nosal <nosalsm@gmail.com>
2022-03-14 16:44:16 -0500

    Completed

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
   // type your code here
+  // writing out 0, 1, 1, 2, 3, 5, 8, etc
+  if (num < 2) {
+    return num
+  }
+  let lastTwo = [0, 1]
+  // num = 4, i = 3
+  for (let i=0; i < num-1; i++) {
+    const sum= lastTwo[0] + lastTwo[1]
+    lastTwo = [lastTwo[1], sum]
+  }
+  return lastTwo[1]
 }
 
 if (require.main === module) {
